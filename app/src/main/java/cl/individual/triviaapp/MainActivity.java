23b4390feeb19d3ab070a56bcaccc19af743950c
Emilia <correoalcantaraemilia@gmail.com>
2023-07-05 14:09:35 -0400

    Creación de un fragmento y nav_graph.

--- app/src/main/java/cl/individual/triviaapp/MainActivity.java
+++ app/src/main/java/cl/individual/triviaapp/MainActivity.java
@@ -1,14 +1,18 @@
-                           package cl.individual.triviaapp;
+package cl.individual.triviaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import cl.individual.triviaapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
